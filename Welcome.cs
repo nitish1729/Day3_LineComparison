5f67e096d74051b47f597905dd4b28a534822899
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-19 22:41:22 +0530

    Welcome to Line Comp..

---
+++ Welcome.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Line Comparison Problem
+
+string Welcome = "Welcome to Line Comparison Computation Program....!";
+
+Console.WriteLine(Welcome);
