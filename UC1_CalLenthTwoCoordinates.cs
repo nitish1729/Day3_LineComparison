3fb602b448ba857d5d37e9bef5498f679613837a
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-19 23:09:05 +0530

    Cal the Length of Two Co-ordin..

---
+++ UC1_CalLenthTwoCoordinates.cs
@@ -0,0 +1,11 @@
+﻿
+// based on a point consisting of(x, y) co-ordinates using the Cartesian system,So that I can calculate its length
+
+
+int x1 = 4;
+int x2 = 2;
+int y1 = 4;
+int y2 = 5;
+double result;
+result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine(result);
