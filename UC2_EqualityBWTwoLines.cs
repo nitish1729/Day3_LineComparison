0a25102ee6e333038934fe6c7504527f3a4892d3
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-19 23:18:46 +0530

    check equality of two lines

---
+++ UC2_EqualityBWTwoLines.cs
@@ -0,0 +1,21 @@
+﻿int x1 = 4;
+int x2 = 2;
+int y1 = 4;
+int y2 = 5;
+double result;
+result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine(result);
+int a1 = 4, a2 = 2;
+int b1 = 4, b2 = 5;
+double result2;
+result2 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+Console.WriteLine(result2);
+
+if (result == result2)
+{
+	Console.WriteLine("both lines are equal");
+}
+else
+{
+	Console.WriteLine("Both lines are not equals");
+}
